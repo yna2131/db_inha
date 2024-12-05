896380ccc1eba59a9323654c5a2f92c6e442262c
Javokhir Yuldoshev <aliyuldashev880@gmail.com>
2024-12-05 17:13:19 +0900

    :sparkles: update TagModule to include forward reference to PostModule and export TagService for improved module interaction

--- backend/src/tags/tag.module.ts
+++ backend/src/tags/tag.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from 'src/post/post.module';
 import { Posts } from 'src/post/post.entity';
 import { TagController } from './tag.controller';
 import { Tags } from './tag.entity';
 import { TagService } from './tag.service';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([Tags]),
-        TypeOrmModule.forFeature([Posts]),
-    ],
-    providers: [TagService],
-    controllers: [TagController],
+  imports: [
+      TypeOrmModule.forFeature([Tags]), 
+      TypeOrmModule.forFeature([Posts]), 
+      forwardRef(() => PostModule),
+  ],
+  providers: [TagService],
+  exports: [TypeOrmModule,TagService], // Export TypeOrmModule so other modules can use TagsRepository
+  controllers: [TagController],
 })
-export class TagModule { }
+export class TagModule {}
