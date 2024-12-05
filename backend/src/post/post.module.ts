02e513ea74b658a65d438387fa8a2dbcc68609b5
Javokhir Yuldoshev <aliyuldashev880@gmail.com>
2024-12-05 17:13:01 +0900

    :sparkles: update PostModule to handle circular dependencies and register Tags entity

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Posts } from './post.entity';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
-import { Posts } from './post.entity';
+import { TagModule } from 'src/tags/tag.module';
+import { Tags } from 'src/tags/tag.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Posts])],
-  providers: [PostService],
+  imports: [
+    TypeOrmModule.forFeature([Posts]), // Register Posts entity
+    TypeOrmModule.forFeature([Tags]), // Register Posts entity
+    forwardRef(() => TagModule), // Handle circular dependencies if needed
+  ],
   controllers: [PostController],
+  providers: [PostService],
+  exports: [TypeOrmModule],
 })
-export class PostModule {}
+export class PostModule { }
